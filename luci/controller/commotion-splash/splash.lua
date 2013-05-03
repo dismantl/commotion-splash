60c75199fa04dc58985ee7fb1b003d86bf60ad5e
Dan Staples <dan.staples@riseup.net>
2013-05-03 11:34:03 -0400

    now you can submit the form

--- luci/controller/commotion-splash/splash.lua
+++ luci/controller/commotion-splash/splash.lua
@@ -1,6 +1,7 @@
 module("luci.controller.commotion-splash.splash", package.seeall)
 
 require "luci.sys"
+require "luci.http"
 require "commotion_helpers"
 
 function index()
@@ -10,11 +11,36 @@ function index()
 end
 
 function config_splash()
-  local ap_iface = luci.sys.exec("ubus call network.interface.ap status |grep '\"device\"' | cut -d '\"' -f 4")
-  local secAp_iface = luci.sys.exec("ubus call network.interface.secAp status |grep '\"device\"' | cut -d '\"' -f 4")
-  local plug_iface = luci.sys.exec("ubus call network.interface.plug status |grep '\"device\"' | cut -d '\"' -f 4")
-  local plug1_iface = luci.sys.exec("ubus call network.interface.plug1 status |grep '\"device\"' | cut -d '\"' -f 4")
-  local plug2_iface = luci.sys.exec("ubus call network.interface.plug2 status |grep '\"device\"' | cut -d '\"' -f 4")
+  local splash = {zones={}, whitelist={}, blacklist={}, subnet={}}
+  local current_ifaces = luci.sys.exec("grep 'GatewayInterface' /etc/nodogsplash/nodogsplash.conf |cut -d ' ' -f 2")
+  local list = list_ifaces()
   
-  luci.template.render("commotion-splash/splash", {ap=ap_iface, secAp=secAp_iface, plug=plug_iface, plug1=plug1_iface, plug2=plug2_iface})
+  -- get current zone(s) set in nodogsplash --> splash.zone_selected
+  for zone, iface in pairs(list.zone_to_iface) do
+    splash.zones[zone]= current_ifaces:match(iface) and "selected" or ""
+  end
+  
+  -- get splash.leasetime
+  splash.leasetime = luci.sys.exec("grep 'ClientIdleTimeout' /etc/nodogsplash/nodogsplash.conf |cut -d ' ' -f 2")
+  
+  -- get whitelist, blacklist, subnet
+  
+  luci.template.render("commotion-splash/splash", splash)
+end
+
+function config_submit()
+  local list = list_ifaces()
+  local new_zone = luci.http.formvalue("cbid.commotion-splash.zone")
+  luci.sys.exec("sed -i -e s/\"^GatewayInterface [[:alnum:]]*$\"/\"GatewayInterface " .. list.zone_to_iface[new_zone] .. "\"/ /etc/nodogsplash/nodogsplash.conf")
+end
+
+function list_ifaces()
+  local r = {zone_to_iface = {}, iface_to_zone = {}}
+  local zones = luci.sys.exec("grep 'config interface' /etc/config/network |cut -d \"'\" -f 2")
+  for zone in zones:gmatch("%w+") do
+    local iface = luci.sys.exec("ubus call network.interface." .. zone .. " status |grep '\"device\"' | cut -d '\"' -f 4")
+    r.zone_to_iface[zone]=iface
+    r.iface_to_zone[iface]=zone
+  end
+  return r
 end
